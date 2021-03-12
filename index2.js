515eb7b218ca8c38b47bfdc8a66088c228f35289
code-driving <tittan7@gmail.com>
2021-03-12 02:05:22 +0000

    promised based approach

---
+++ index2.js
@@ -0,0 +1,20 @@
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It did not work: ", error.message);
+  });
+  
+  //code in this module above taken from compass for learning purposes
